b2fd2967bb03cb83608d5b485f5fbfa0306f2cc3
kennyjosef <idowukehinde28@gmail.com>
2022-02-22 12:48:41 -0800

    changes

---
+++ src/Components/MenuItem.js
@@ -0,0 +1,27 @@
+export const MenuItems=[
+    {
+        title: 'Home',
+        url:'#',
+        cName:'nav-link'
+    },
+    {
+        title: 'About',
+        url:'#',
+        cName:'nav-link'
+    },
+    {
+        title: 'Contact',
+        url:'#',
+        cName:'nav-link'
+    },
+    {
+        title: 'Blog',
+        url:'#',
+        cName:'nav-link'
+    },
+    {
+        title: 'Career',
+        url:'#',
+        cName:'nav-link'
+    },
+]
