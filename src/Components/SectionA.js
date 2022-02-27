73fcb4ca8f3eaca0f3c0421488a28efdc163e5ad
kennyjosef <idowukehinde28@gmail.com>
2022-02-27 15:07:14 -0800

    changes

--- src/Components/SectionA.js
+++ src/Components/SectionA.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 import Logo2 from '../Assest/image-mockups.png';
-// import Logo2 from '../Assest/bg-intro-mobile.svg';
-// import Logo2 from '../Assest/bg-intro-mobile.svg';
+
 
 import Button from './Button';
 import  './style.css';
@@ -9,7 +9,18 @@ const Content = () => {
     return ( 
         <div className="section">
             <div className='items'>
-                <h3>Next generation digital banking</h3>
+                <h3>Next generation digital 
+                    <span style={{color:"red", fontWeight:"bold", marginLeft:'10px'}}>
+                        <Typewriter
+                        loop
+                        cursor
+                        cursorStyle=''
+                        typeSpeed={70}
+                        delaySpeed={50}
+                        delaySpeed={1000}
+                        words={["banking", "marketing", "business", "networking"]}/>
+                    </span>
+                </h3>
                 <p>Take your financial life online. Your Easybank account will be
                     a one-stop-shop for spending, saving, budgeting, investing and much more.
                 </p>
